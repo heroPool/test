ccad05020c73e969a2b51b6f2a9edaa455855990
zhuHuaJian <935625755@qq>
2017-04-20 15:00:54 +0800

    1

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iniView();
+    }
 
+    private void iniView() {
+        String str = "123";
     }
 }
